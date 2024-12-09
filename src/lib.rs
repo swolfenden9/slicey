3ef1dcaa51a564ca5974bf566f218f2111062d50
Sol Wolf <wolfendenbuild@gmail.com>
2024-12-09 19:13:40 +1000

    Change [`Spanned`] and [`Sliced`] to [`struct@Spanned`] and [`struct@Sliced`] in lib.rs docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 //! # Slicey
 //!
 //! Slicey provides two simple ways to associate data with parts of a string:
-//! - [`Spanned`]: Represents data and a range.
-//! - [`Sliced`]: Represents data, a range, and a source string.
+//! - [`struct@Spanned`]: Represents data and a range.
+//! - [`struct@Sliced`]: Represents data, a range, and a source string.
 //!
 //! See their documentation for more info.
 
